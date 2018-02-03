85cf97654d277b12f66c5feb073406186e94b3c1
alexp <alexplotbox@teamlead.ru>
2018-02-03 11:37:19 +0300

    init

---
+++ server.js
@@ -0,0 +1,22 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(client){
+  console.log('a user connected');
+
+  client.on('subscribeToTimer', (interval) => {
+    console.log('client is subscribing to timer with interval ', interval);
+    setInterval(() => {
+      client.emit('timer', new Date());
+    }, interval);
+  });
+});
+
+http.listen(3001, function(){
+  console.log('listening on *:3001');
+});
