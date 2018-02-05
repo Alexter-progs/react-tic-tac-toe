fb00372ace9c5ee01a21078231ce30e6d06fafda
alexp <alexplotbox@teamlead.ru>
2018-02-05 20:01:21 +0300

    remove some CRA generated code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import openSocket from 'socket.io-client';
 
@@ -24,16 +23,8 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>{this.state.timestamp}</p>
-        <p>{process.env.REACT_APP_HOST_URL}</p>
+      <div>
+        Hello
       </div>
     );
   }
