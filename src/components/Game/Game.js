74585cc834a65c1f88321e5a9f6f64445a728702
alexp <alexplotbox@teamlead.ru>
2018-02-06 23:04:22 +0300

    remove classname prop

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,17 +1,72 @@
 import React, { Component } from 'react';
 import './Game.css';
 
-import { socket } from '../../api';
+import { socket, onEnemyStep } from '../../api';
+
+class Cell extends Component {
+    state = {
+        mark: ''
+    }
+
+    handleStep = () => {
+        const { x, y } = this.props;
+
+        socket.emit('step', { x, y }, () => {
+            this.setState({
+                mark: 'X'
+            });
+        });
+    }
+
+    
+
+    render() {
+        const { x, y } = this.props;
+        const { mark } = this.state;
+
+        return (
+            <div className="cell" onClick={this.handleStep}>
+                <span className="cell-text">
+                    { mark }
+                </span>
+            </div>
+        )
+    }
+}
+
+const Grid = () => {
+    let grid = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ]
+
+    return grid.map((row, rowIndex) => {
+        return row.map((cell, columnIndex) => {
+            return <Cell key={rowIndex + columnIndex} x={rowIndex} y={columnIndex}/>
+        })  
+    }); 
+}
 
 class Game extends Component {
-  render() {
-    const { className: gameClassName } = this.props;
-    return (
-      <div className="game">
-        YO
-      </div>
-    );
-  }
+    constructor() {
+        super();
+
+        onEnemyStep(({ x, y }) => {
+            console.log(`x: ${x}, y: ${y}`);
+        })
+    }
+
+    render() {
+        const { className: gameClassName, isFirst } = this.props;
+        const mark = isFirst ? 'X' : 'O';
+
+        return (
+        <div className="game">
+            <Grid/>
+        </div>
+        );
+    }
 }
 
 export default Game;
