ee8c75780d81e26b3bd255b7c8712cfafc39766d
alexp <alexplotbox@teamlead.ru>
2018-02-07 13:31:10 +0300

    style chat and a bit game grid

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -3,6 +3,11 @@ import './Chat.css';
 
 import { onChatMessage, socket } from '../../api'
 
+// Generates messages id for key prop, 
+// otherwise animations won't work because 
+// react is not updating elements
+import uuid from 'uuid';
+
 class Chat extends Component {
   state = {
     message: '',
@@ -14,7 +19,7 @@ class Chat extends Component {
 
     onChatMessage((message) => {
       console.log(`Received message: ${message}`)
-      this.updateMessages({ text: message, user: 'Oponent' });
+      this.updateMessages({ text: message, user: 'Oponent', id: uuid.v4() });
     })
   }
 
@@ -36,7 +41,7 @@ class Chat extends Component {
     const { message } = this.state;
 
     if(message.length > 0) {
-      this.updateMessages({ text: message, user: 'Me' });
+      this.updateMessages({ text: message, user: 'Me', id: uuid.v4() });
       socket.emit('broadcastMessage', message);
     }
   }
@@ -55,8 +60,11 @@ class Chat extends Component {
     }));
   }
 
+  // Faking queue
   pop = (arr) => {
-    if(arr.length > 10) {
+    const MAX_MESSAGE_COUNT = 21;
+
+    if(arr.length > MAX_MESSAGE_COUNT) {
       arr.reverse();
       arr.pop();
       arr.reverse();
@@ -71,13 +79,13 @@ class Chat extends Component {
       <div className="chat">
         <div className="chat-messages">
           {messages.map((message, index) => (
-            <span key={index}>
+            <span key={message.id}>
               <p>{message.user}: {message.text}</p>
             </span>
           ))}
         </div>
         <div className="chat-input">
-          <div className="message-wrapper">
+          <div className="message-input-wrapper">
             <input type="text" placeholder="Enter your message"
               value={this.state.message} 
               onChange={this.messageChange}
